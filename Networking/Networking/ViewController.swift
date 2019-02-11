d2fa15a4884a9d12b64f118124f78d0fa14e9529
Harendra <harendra2008gwl@gmail.com>
2019-02-11 18:24:00 +0530

    Initial setup pod installed
    Signed-off-by: Harendra Sharma <harendra.sharma@iskpro.com>

---
+++ Networking/Networking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Harendra Sharma on 11/02/19.
+//  Copyright © 2019 Harendra Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
