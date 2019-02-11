df568fd3a1dffc5cb07de36469b06c1c4eb47fc8
Harendra Sharma <harendra.sharma@iskpro.com>
2019-02-11 19:52:33 +0530

    Stable code

--- Networking/Networking/ViewController.swift
+++ Networking/Networking/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import Alamofire
+import SVProgressHUD
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        MakeRequest()
 
-
+    
 }
 
+    
+    func MakeRequest()  {
+        SVProgressHUD.show(withStatus: "Loading..")
+        
+        
+        
+        AF.request("https://reqres.in/api/users").responseJSON { response in
+//            print("Request: \(String(describing: response.request))")   // original url request
+//            print("Response: \(String(describing: response.response))") // http url response
+//            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+            
+            SVProgressHUD.dismiss()
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
